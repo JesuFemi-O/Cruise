3c6a78462802a639d46a589f130501cae623fda9
JesuFemi-O <ogunwedeemmanuel@gmail.com>
2021-08-02 00:40:14 +0100

    update entry point

--- tf_serving_entrypoint.sh
+++ tf_serving_entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+mkdir /default
+cd /default
+
+curl $TENSORFLOW_MODEL_URL --output default.tar.gz
+tar -zxvf default.tar.gz
+mv $MODEL_NAME default
+
 tensorflow_model_server --port=8500 --rest_api_port="${PORT}" --model_name="${MODEL_NAME}" --model_base_path="${MODEL_BASE_PATH}"/"${MODEL_NAME}" "$@"
