0f113ebb24d69bcfc37f3bc2599e7e35a78b67ee
Emmanuel Ogunwede <47831972+JesuFemi-O@users.noreply.github.com>
2021-08-02 01:22:53 +0100

    Tweaking sh file

--- tf_serving_entrypoint.sh
+++ tf_serving_entrypoint.sh
@@ -5,6 +5,6 @@ cd /default
 
 curl $TENSORFLOW_MODEL_URL --output default.tar.gz
 tar -zxvf default.tar.gz
-mv $MODEL_NAME default
+mv $MODEL_FOLDER_NAME default
 
 tensorflow_model_server --port=8500 --rest_api_port="${PORT}" --model_name="${MODEL_NAME}" --model_base_path="${MODEL_BASE_PATH}"/"${MODEL_NAME}" "$@"
