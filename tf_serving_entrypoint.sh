16f06da05c4ed1a31ae4e29d42393bf0fee53e24
Emmanuel Ogunwede <47831972+JesuFemi-O@users.noreply.github.com>
2021-09-01 05:11:19 +0100

    moved server init command to if block.

--- tf_serving_entrypoint.sh
+++ tf_serving_entrypoint.sh
@@ -6,9 +6,11 @@ curl $TENSORFLOW_MODEL_URL --output $DEFAULT_FILE_NAME
 OUTPUT=$(file --mime-type $DEFAULT_FILE_NAME)
 
 if [[ $OUTPUT =~ "application/gzip" ]]
-    then tar -zxvf $DEFAULT_FILE_NAME
+then 
+    tar -zxvf $DEFAULT_FILE_NAME
+    tensorflow_model_server --port=8500 --rest_api_port="${PORT}" --model_config_file=/"${MODEL_BASE_PATH}"/"${MODEL_CONFIG_FILE}"  "$@"
 else
-    echo "File is of $OUTPUT format"
+    echo "File is of ${OUTPUT} format"
 fi
 
-tensorflow_model_server --port=8500 --rest_api_port="${PORT}" --model_config_file=/"${MODEL_BASE_PATH}"/"${MODEL_CONFIG_FILE}"  "$@"
+
